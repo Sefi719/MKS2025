b6b284534738eb3b858076f419bc8eb31c5fd53c
Sefi719 <240869@vutbr.cz>
2025-09-24 12:55:03 +0200

    MKS final

--- MKS1/Core/Src/main.c
+++ MKS1/Core/Src/main.c
@@ -90,7 +90,49 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+  //uint8_t array[32]= {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
 
+  uint32_t array=0b10101001110111011100101010000000;
+  while (1)
+  {
+	  /* for (uint8_t i=0; i<32; i++)
+	  {
+		  if(array[i])
+		  {
+			  LL_GPIO_SetOutputPin(LD2_GPIO_Port, LD2_Pin);
+			  LL_mDelay(200);
+		  }
+		  else
+		  {
+			  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
+			  LL_mDelay(200);
+		  }
+
+
+	  }
+	  */
+	  for (uint8_t i=0; i<32; i++)
+	  	  {
+	  		  if((array>>i)&1)
+	  		  {
+	  			  LL_GPIO_SetOutputPin(LD2_GPIO_Port, LD2_Pin);
+	  			  LL_mDelay(200);
+	  		  }
+	  		  else
+	  		  {
+	  			  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
+	  			  LL_mDelay(200);
+	  		  }
+
+
+	  	  }
+
+
+	  /*if(i<29) i++;
+	  else i=0;
+	  LL_mDelay(200);*/
+
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
